1f5456a79d104bd0d6a3645cc793f917ad8547f8
Andreas Wortmann <andreas@e247-221.eduroam.tuwien.ac.at>
2017-03-03 11:26:35 +0100

    Removed old version

--- ur1.diverse.montiarc.xdsml.k3dsa/xtend-gen/ur1/diverse/montiarc/xdsml/montiarc/aspects/PortInstanceAspectPortInstanceAspectProperties.java
+++
@@ -1,8 +0,0 @@
-package ur1.diverse.montiarc.xdsml.montiarc.aspects;
-
-import montiarcruntime.PortValue;
-
-@SuppressWarnings("all")
-public class PortInstanceAspectPortInstanceAspectProperties {
-  public PortValue portValue;
-}
