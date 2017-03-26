59cc12b6f84833240ce5dc7f546f8a1eb108eaab
Andreas Wortmann <andreas@Andreass-MacBook-Pro.local>
2017-03-26 20:56:13 +0200

    deleted generated files, updated gitignores

--- ur1.diverse.xmontiarc.k3dsa/xtend-gen/ur1/diverse/xmontiarc/k3dsa/xmontiarc/aspects/PortAspectPortAspectProperties.java
+++
@@ -1,8 +0,0 @@
-package ur1.diverse.xmontiarc.k3dsa.xmontiarc.aspects;
-
-import org.eclipse.emf.ecore.EObject;
-
-@SuppressWarnings("all")
-public class PortAspectPortAspectProperties {
-  public EObject value;
-}
