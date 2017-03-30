e8c472ab52183b579e42fb95f9571d83f7065271
Olivier Barais <barais@irisa.fr>
2017-03-30 02:05:12 +0200

    groovy script integration

--- ur1.diverse.xmontiarc.runtime/src/ur1/diverse/xmontiarc/runtime/GroovyInterpreter.java
+++ ur1.diverse.xmontiarc.runtime/src/ur1/diverse/xmontiarc/runtime/GroovyInterpreter.java
@@ -6,6 +6,9 @@ import org.eclipse.emf.ecore.EDataType;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EcorePackage;
 
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
 /**
  * @generated NOT
  */
@@ -14,8 +17,21 @@ public class GroovyInterpreter {
 	private static Random rand = new Random();
 	
 	public static EObject interpret(String groovyScript) {
-		EDataType data =  EcorePackage.eINSTANCE.getEString();
-		data.setName(""+(rand.nextInt(100)+1));
+		EDataType data =  EcorePackage.eINSTANCE.getEString();				
+		Binding binding = new Binding();
+
+		//Binding setVariable allow to pass a variable from the moonti arc model to the groovy interpreter
+		//binding.setVariable(name, value);
+		
+		 GroovyShell shell = new GroovyShell(binding);		    
+		    Object result = shell.evaluate(groovyScript);
+		    
+		    //Binding.getVariable get the new value of this variable. 
+//		    binding.getVariable(name)
+
+		    
+		    //		data.setName(""+(rand.nextInt(100)+1));
+		data.setName(""+result);
 		return data;
 	}
 }
